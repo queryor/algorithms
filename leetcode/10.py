54678f9d7a20f18e03a39966d17ce615bcaef6e2
lab_win10 <queryor.163.com>
2019-02-22 16:36:02 +0800

    10

---
+++ leetcode/10.py
@@ -0,0 +1,115 @@
+# Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+# '.' Matches any single character.
+# '*' Matches zero or more of the preceding element.
+# The matching should cover the entire input string (not partial).
+
+# Note:
+
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like . or *.
+# Example 1:
+
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+# Example 2:
+
+# Input:
+# s = "aa"
+# p = "a*"
+# Output: true
+# Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+# Example 3:
+
+# Input:
+# s = "ab"
+# p = ".*"
+# Output: true
+# Explanation: ".*" means "zero or more (*) of any character (.)".
+# Example 4:
+
+# Input:
+# s = "aab"
+# p = "c*a*b"
+# Output: true
+# Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+# Example 5:
+
+# Input:
+# s = "mississippi"
+# p = "mis*is*p*."
+# Output: false
+
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if len(p)==0: 
+            return len(s)==0
+        elif len(p)==1:
+            if len(s)==1 and (p[0]==s[0] or p[0]=='.'):
+                return True
+            else:
+                return False
+        elif p[1]!='*':
+            if len(s)==0:
+                return False
+            else:
+                return(s[0]==p[0] or p[0]=='.') and self.isMatch(s[1:],p[1:])
+        else:
+            while(len(s)!=0 and (s[0]==p[0] or p[0]=='.')):
+                if(self.isMatch(s,p[2:])):
+                    return True
+                s = s[1:]
+        return self.isMatch(s,p[2:])
+    def isMatch1(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if len(p)==0: 
+            return len(s)==0
+        if(len(p)>1 and p[1]=='*'):
+            return self.isMatch(s,p[2:]) or (len(s)!=0 and(s[0]==p[0] or p[0]=='.')) and self.isMatch(s[1:],p)
+        else:
+            return len(s)!=0 and (s[0]==p[0] or p[0]=='.') and self.isMatch(s[1:],p[1:])
+    def __init__(self):
+        self.cache = {}
+    def isMatch2(self, s: 'str', p: 'str') -> 'bool':
+        if len(p) == 0:     return len(s) == 0
+        question = s + '+' + p
+        if question in self.cache:
+            return self.cache[question]
+        firstMatch = len(s) > 0 and (p[0] == s[0] or p[0] == '.')
+        if len(p) > 1 and p[1] == '*':
+            if firstMatch:
+                res = self.isMatch(s[1:], p) or self.isMatch(s, p[2:])
+            else:
+                res = self.isMatch(s, p[2:])
+        else:
+            res = firstMatch and self.isMatch(s[1:], p[1:])
+        self.cache[question] = res
+        return res
+    def isMatch3(self,s,p):
+        dp = [[False for i in range(len(p)+1)] for i in range(len(s)+1)]
+        dp[0][0] = True
+        for i in range(len(s)+1):
+            for j in range(1,len(p)+1):
+                if(j>1 and p[j-1]=='*'):
+                    dp[i][j]=dp[i][j-2] or (dp[i-1][j] and (i>0 and (p[j-2]==s[i-1] or p[j-2]=='.')))
+                else: 
+                    dp[i][j]=dp[i-1][j-1] and i>0 and (s[i-1] == p[j-1] or p[j-1]=='.')
+        print(dp)
+        return dp[len(s)][len(p)]
+s  = Solution()
+a = "aaa"
+p = "a*"
+print(s.isMatch3(a,p))
+
