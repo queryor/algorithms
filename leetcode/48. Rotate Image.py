e4b30c69dae451e6a55fb52129bf2c8a8c5fa289
lab_win10 <queryor.163.com>
2019-03-08 11:19:16 +0800

    48

--- leetcode/48. Rotate Image.py
+++ leetcode/48. Rotate Image.py
@@ -40,7 +40,7 @@
 # ]
 
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
