f0e4115e787e229875b76cb77842c394404bb1be
lab_win10 <queryor.163.com>
2019-03-12 11:25:14 +0800

    67

--- leetcode/67. Add Binary.py
+++ leetcode/67. Add Binary.py
@@ -14,7 +14,15 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return str(bin(int(a, 2) + int(b, 2)))[2:]
-    
+    def addBinary1(self, a, b):
+        if len(a)==0: return b
+        if len(b)==0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[0:-1],b[0:-1]),'1')+'0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[0:-1],b[0:-1])+'0'
+        else:
+            return self.addBinary(a[0:-1],b[0:-1])+'1'
 
 s = Solution()
 a = '11'
