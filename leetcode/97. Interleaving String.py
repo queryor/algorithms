cad627384a0f19b283048254720d07798ecd3ca2
lab_win10 <queryor.163.com>
2019-03-15 18:02:49 +0800

    97 not done

--- leetcode/97. Interleaving String.py
+++ leetcode/97. Interleaving String.py
@@ -8,9 +8,9 @@
 
 # Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 # Output: false
-
+#   只要是遇到字符串的子序列或是匹配问题直接就上动态规划Dynamic Programming，其他的都不要考虑
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         pass
-
+        pass
         
