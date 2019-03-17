86adb5fa5e8cba629cbeca10db4e61920b87f3db
lab_win10 <queryor.163.com>
2019-03-17 19:01:16 +0800

    97

--- leetcode/97. Interleaving String.py
+++ leetcode/97. Interleaving String.py
@@ -11,6 +11,23 @@
 #   只要是遇到字符串的子序列或是匹配问题直接就上动态规划Dynamic Programming，其他的都不要考虑
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        pass
-        pass
-        
+        # 16%
+        if len(s1)+len(s2)!=len(s3):
+            return False
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = [[0 for i in range(n2+1)]for i in range(n1+1)]
+        dp[0][0]=True
+        #s2 为空
+        for i in range(1,n1+1):
+            dp[i][0]=dp[i-1][0] and s1[i-1]==s3[i-1]
+        # s1 为空
+        for i in range(1,n2+1):
+            dp[0][i]=dp[0][i-1] and s2[i-1]==s3[i-1]
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                dp[i][j]=((dp[i-1][j]) and s1[i-1]==s3[i-1+j]) \
+                     or (dp[i][j-1] and s2[j-1]==s3[j-1+i])
+        return dp[n1][n2]       
+
+    ### DFS 和 BFS 解法未解决
