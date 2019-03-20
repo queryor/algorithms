32526249488f1985a3ee545f46a827c6bd78573f
lab_win10 <queryor.163.com>
2019-03-20 09:41:21 +0800

    105

---
+++ leetcode/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,28 @@
+# Given preorder and inorder traversal of a tree, construct the binary tree.
+
+# Note:
+# You may assume that duplicates do not exist in the tree.
+
+# For example, given
+
+# preorder = [3,9,20,15,7]
+# inorder = [9,3,15,20,7]
+# Return the following binary tree:
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# Accepted
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder) -> TreeNode:
+        pass
+        
