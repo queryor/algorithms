563af6942295f61c0822785c84ca3c72c0d4ae11
lab_win10 <queryor.163.com>
2019-03-22 17:14:31 +0800

    118

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/pascals-triangle/
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        res = []
+        ans = [1]
+        res.append(ans)
+        if numRows == 1:
+            return res
+        for i in range(2,numRows+1):
+            ans = [1 for j in range(i)]
+            for j in range(1,i-1):
+                ans[j]=res[-1][j]+res[-1][j-1]
+            res.append(ans)
+        return res
