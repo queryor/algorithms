f591356930a3f7652b42e049465a14348a3047a9
lab_win10 <queryor.163.com>
2019-03-27 15:17:19 +0800

    99

--- leetcode/99. Recover Binary Search Tree.py
+++ leetcode/99. Recover Binary Search Tree.py
@@ -41,3 +41,33 @@
 # A solution using O(n) space is pretty straight forward.
 # Could you devise a constant space solution?
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # 对于此问题，关键是要找到被调换的两个元素。我们知道，对于中序遍历，合法的BST数必为有序，故进行中序遍历，
+        # 如果pre.val >root.val说明有误。注意区分第一次和第二次，标记即可
+        self.pre,self.first,self.second=None,None,None
+        self.dfs(root)
+        t=self.first.val
+        self.first.val,self.second.val = self.second.val,t
+ 
+    def dfs(self,root):
+        if not root: return
+        if root.left:  self.dfs(root.left)
+        
+        if self.pre:
+            if self.pre.val > root.val : 
+                if not self.first :self.first,self.second=self.pre,root
+                else:   self.second=root
+ 
+        self.pre=root
+        if root.right: self.dfs(root.right)
