9cec39f8da262f4434eaf505c6ee013f328a0cfa
lab_win10 <queryor.163.com>
2019-05-09 12:03:51 +0800

    127

--- leetcode/127. Word Ladder.py
+++ leetcode/127. Word Ladder.py
@@ -31,3 +31,34 @@
 
 # Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
 
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList) -> int:
+        wordset  = set(wordList)
+        if endWord not in wordset:
+            return 0
+        quene = [beginWord]
+        res = 0
+        while(len(quene)!=0):
+            for k in range(len(quene)):
+                word = quene[0]
+                if word == endWord:
+                    return res+1
+                quene = quene[1:]
+                for j in range(len(word)):
+                    for c in range(ord('a'),ord('z')+1):
+                        newword = [i for i in word]
+                        newword[j] = chr(c)
+                        newword = "".join(newword.copy())
+                        if str(newword) != word and newword in wordset:
+                            quene.append(newword)
+                            wordset.remove(newword)
+            res+=1
+        return 0
+        
+
+
+s = Solution()
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+print(s.ladderLength(beginWord,endWord,wordList))
