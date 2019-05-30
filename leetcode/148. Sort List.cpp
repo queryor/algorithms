594416ee28181c838ad94dfabd09d6198f2b09ac
lab_win10 <queryor.163.com>
2019-05-30 15:27:07 +0800

    148

--- leetcode/148. Sort List.cpp
+++ leetcode/148. Sort List.cpp
@@ -9,15 +9,15 @@
 using namespace std;
 
 
+.
 
-// Definition for a binary tree node.
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
 
 class Solution {
 public:
