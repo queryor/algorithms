b809c5f13b3baf7255c45edf0db671285655978f
lab_win10 <queryor.163.com>
2019-06-26 20:14:10 +0800

    162

---
+++ leetcode/162. Find Peak Element.py
@@ -0,0 +1,48 @@
+'''峰值元素是指其值大于左右相邻值的元素。
+
+给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+
+数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+
+你可以假设 nums[-1] = nums[n] = -∞。
+
+示例 1:
+
+输入: nums = [1,2,3,1]
+输出: 2
+解释: 3 是峰值元素，你的函数应该返回其索引 2。
+示例 2:
+
+输入: nums = [1,2,1,3,5,6,4]
+输出: 1 或 5 
+解释: 你的函数可以返回索引 1，其峰值元素为 2；
+     或者返回索引 5， 其峰值元素为 6。
+说明:
+
+你的解法应该是 O(logN) 时间复杂度的。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-peak-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+class Solution(object):
+    def findPeakElement(self, m):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(m)
+        left = 0
+        right = n-1
+        while left<right:
+            mid = (right+left)//2
+            val = m[mid]
+            pre = m[mid-1]
+            last = m[mid+1]
+            if val>pre and val>last:
+                return mid
+            if val<last:
+                left = mid+1
+            else: 
+                right = mid-1
+        return left
